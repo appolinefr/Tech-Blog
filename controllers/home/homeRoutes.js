bd391f3651ab251569a0ec534cece8ba3055ec72
Appoline Cogan <appolinecogan@gmail.com>
2022-09-23 11:50:51 +0200

    updated views and removed unecessary files

--- controllers/home/homeRoutes.js
+++ controllers/home/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-// const { BlogPost, User } = require("../../models");
-// const withAuth = require("../../utils/auth");
+const { BlogPost, User } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
@@ -15,21 +15,21 @@ router.get("/signUp", async (req, res) => {
   else res.render("signUp");
 });
 
-// router.get("/blogPost", withAuth, async (req, res) => {
-//   try {
-//     const userData = await User.findOne({ where: { id: req.session.userId } });
-//     const user = userData.get({ plain: true });
+router.get("/blogPost", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { id: req.session.userId } });
+    const user = userData.get({ plain: true });
 
-//     const blogPostData = await BlogPost.findAll({ where: { userId: user.id } });
-//     const blogPosts = blogPostData.map((data) => data.get({ plain: true }));
+    const blogPostData = await BlogPost.findAll({ where: { userId: user.id } });
+    const blogPosts = blogPostData.map((data) => data.get({ plain: true }));
 
-//     res.render("blogPost", {
-//       blogPosts,
-//       userName: user.name,
-//       loggedIn: req.session.loggedIn,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render("blogPost", {
+      blogPosts,
+      userName: user.name,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
